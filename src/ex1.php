bfcb08b9e898cc911bfc561353c1c7367fb22725
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-02-02 14:46:28 +0200

    Working on Exercise1

--- src/ex1.php
+++ src/ex1.php
@@ -1,3 +1,10 @@
-<?php
-echo "<h1>Hello World! My name is \"Subhanjan\" :)</h1>"
-?>
+<html>
+<body>
+    <h1> Exercise: 1 Getting Started with PHP - Subhanjan </h1>
+    <?php
+        echo "<h2>Hello World! My name is \"Subhanjan\" :)</h2>";
+        $title = "PHP is interesting";
+        echo "<h3> $title </h3>";
+    ?>
+</body>
+</html>
