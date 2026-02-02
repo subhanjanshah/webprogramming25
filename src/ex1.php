beae84e9c61ad4cdad354d8a0649578986825a83
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-02-02 15:03:40 +0200

    Continued with Exercise1

--- src/ex1.php
+++ src/ex1.php
@@ -5,6 +5,32 @@
         echo "<h2>Hello World! My name is \"Subhanjan\" :)</h2>";
         $title = "PHP is interesting";
         echo "<h3> $title </h3>";
+    
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+
+   echo 
+   "<table>
+        <tr>
+            <td> S.N.</td>
+            <td> John </td>
+            <td> $g1 </td>
+        </tr>
+
+        <tr>
+            <td> S.N.</td>
+            <td> Alice </td>
+            <td> $g2 </td>
+        </tr>
+
+        <tr>
+            <td> S.N.</td>
+            <td> Bob </td>
+            <td> $g3 </td>
+        </tr>
+    </table>";
+    echo "<img width= 650 px src = \"./Ex1.png\" >";
     ?>
 </body>
 </html>
