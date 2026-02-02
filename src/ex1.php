eba36a23da6419f709435ff44f18be6a83a07083
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-02-02 15:29:25 +0200

    Fixed Indentations

--- src/ex1.php
+++ src/ex1.php
@@ -1,54 +1,57 @@
 <html>
 <body>
-     <style>
-    table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 10%;
-}
+    <style>
+    table 
+        {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 10%;
+        }
 
-tr, td {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+    tr, td 
+        {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
     </style>
-    <h1> Exercise: 1 Getting Started with PHP - Subhanjan </h1>
+        <h1> Exercise: 1 Getting Started with PHP - Subhanjan </h1>
     <?php
-        echo "<h2>Hello World! My name is \"Subhanjan\" :)</h2>";
-        $title = "PHP is interesting";
-        echo "<h3> $title </h3>";
+            echo "<h2>Hello World! My name is \"Subhanjan\" :)</h2>";
+            $title = "PHP is interesting";
+            echo "<h3> $title </h3>";
     
-    $g1 = 5;
-    $g2 = 4;
-    $g3 = 5;
+            $g1 = 5;
+            $g2 = 4;
+            $g3 = 5;
 
-   echo 
-   "<table>
-        <tr>
-            <td> S.N.</td>
-            <td> Name </td>
-            <td> Grade </td>
-        </tr>
-        <tr>
-            <td> 1 </td>
-            <td> John </td>
-            <td> $g1 </td>
-        </tr>
+            echo "<table>
+                        <tr>
+                            <td> S.N.</td>
+                            <td> Name </td>
+                            <td> Grade </td>
+                        </tr>
 
-        <tr>
-            <td> 2 </td>
-            <td> Alice </td>
-            <td> $g2 </td>
-        </tr>
+                        <tr>
+                            <td> 1 </td>
+                            <td> John </td>
+                            <td> $g1 </td>
+                        </tr>
 
-        <tr>
-            <td> 3 </td>
-            <td> Bob </td>
-            <td> $g3 </td>
-        </tr>
-    </table>";
-    echo "<img width= 650 px src = \"./Exercise1.png\" >";
+                        <tr>
+                            <td> 2 </td>
+                            <td> Alice </td>
+                            <td> $g2 </td>
+                        </tr>
+
+                        <tr>
+                            <td> 3  </td>
+                            <td> Bob </td>
+                            <td> $g3 </td>
+                        </tr>
+                </table>";
+
+            echo "<img width= 650 px src = \"./Exercise1.png\" >";
     ?>
 </body>
 </html>
