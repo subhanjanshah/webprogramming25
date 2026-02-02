f41672d55fb61f50080058909abf8ebca99e67be
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-02-02 15:16:55 +0200

    Some changes in the Exercise1

--- src/ex1.php
+++ src/ex1.php
@@ -1,5 +1,18 @@
 <html>
 <body>
+     <style>
+    table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 10%;
+}
+
+tr, td {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+    </style>
     <h1> Exercise: 1 Getting Started with PHP - Subhanjan </h1>
     <?php
         echo "<h2>Hello World! My name is \"Subhanjan\" :)</h2>";
@@ -14,23 +27,28 @@
    "<table>
         <tr>
             <td> S.N.</td>
+            <td> Name </td>
+            <td> Grade </td>
+        </tr>
+        <tr>
+            <td> 1 </td>
             <td> John </td>
             <td> $g1 </td>
         </tr>
 
         <tr>
-            <td> S.N.</td>
+            <td> 2 </td>
             <td> Alice </td>
             <td> $g2 </td>
         </tr>
 
         <tr>
-            <td> S.N.</td>
+            <td> 3 </td>
             <td> Bob </td>
             <td> $g3 </td>
         </tr>
     </table>";
-    echo "<img width= 650 px src = \"./Ex1.png\" >";
+    echo "<img width= 650 px src = \"./Exercise1.png\" >";
     ?>
 </body>
 </html>
