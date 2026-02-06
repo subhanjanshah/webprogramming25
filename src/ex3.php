c6ec5e1782035410bee309c904c2c97fb130c2b6
subhanjanshah289-hash <subhanjan.shah@student.hamk.fi>
2026-02-06 14:51:32 +0200

    Applying bootstrap!

--- src/ex3.php
+++ src/ex3.php
@@ -6,13 +6,23 @@ include 'header.php';
 ?>
 
 <html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
 <body>
 <form action="./formprint.php" method="post">
-First name: <input type="text" name="name"> <br>
-Last name: <input type="text" name="name1"> <br>
-<input href="./formprint.php" type="submit">
-</form>
-
+<div class="mb-3">
+            <label class="form-label">First name:</label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Last name:</label>
+            <input type="text" class="form-control" name="name1"> 
+        </div>
+        <input type="submit" class="btn btn-primary">
+    </form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php include 'footer.php' ?>
